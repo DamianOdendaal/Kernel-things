6a7420eace610bffe59fcfcc283c39213e34ddc3
Damian ODENDAAL <dodendaa@c5r10s3.wethinkcode.co.za>
2020-01-08 16:46:45 +0200

    got the stuff working  nice. Fixing readme too

--- Hello World /Hello_World.c
+++ Hello World /Hello_World.c
@@ -7,3 +7,13 @@ static int __init hello_world(void)
     printk(KERN_INFO "Hello world, its Mr Dodo here\n");
     return 0;
 }
+
+static void __exit goodbye_world(void)
+{
+    printk(KERN_INFO "Goodbye world!, Mr Dodo is leaving\n");
+}
+
+module_init(hello_world);
+module_exit(goodbye_world);
+
+// continue watching from 10:02 on  https://www.youtube.com/watch?v=U5G_rT-GwPE
