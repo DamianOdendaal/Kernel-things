cb5c468b0eed1ceaa4f88562e8cedb91658980a4
Damian <damianodendaal@f-secure.com>
2020-05-26 11:17:03 +0200

    took out extra comments

--- Hello_world/hello_world.c
+++ Hello_world/hello_world.c
@@ -1,14 +1,14 @@
-#include <linux/modlue.h>           // this provides all the module definitions i will need in module , also to recognise that it is a module
-#include <linux/kernel.h>           // this provides the core functions you will find in the kernel  
+#include <linux/modlue.h>          
+#include <linux/kernel.h>            
 #include <linux/init.h>
 
-static int __init hello_world(void)     // the method needs to be static because there must only be one instance of it.
+static int __init hello_world(void)    
 {
-    printk(KERN_INFO "Hello world, its Mr Dodo here\n");        // instead of printf for print to file we use printk to print to a log file which can be displayed by using dmesg to view log files 
+    printk(KERN_INFO "Hello world, its Mr Dodo here\n"); 
     return 0;
 }
 
-static void __exit goodbye_world(void)      // this is to deallocate resources that we have used
+static void __exit goodbye_world(void) 
 {
     printk(KERN_INFO "Goodbye world!, Mr Dodo is leaving\n");
 }
